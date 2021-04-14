4fd77502c0f922dd23d854c20c397421047c8680
j <womeiyouleezi@github.com>
2021-04-14 16:44:04 +0000

    testing, fixed small bug

--- train.py
+++ train.py
@@ -104,7 +104,7 @@ if __name__ == "__main__":
             optimizer = torch.optim.Adam(model.parameters(), lr=args.learning_rate, betas=beta_tuple, weight_decay=args.Adam_weight_decay)
         elif optimizer_type == 'AdamW':
             beta_tuple = (args.Adam_beta_1, args.Adam_beta_2)
-            optimizer = torch.optim.Adam(model.parameters(), lr=args.learning_rate, betas=beta_tuple, weight_decay=args.Adam_weight_decay)
+            optimizer = torch.optim.AdamW(model.parameters(), lr=args.learning_rate, betas=beta_tuple, weight_decay=args.Adam_weight_decay)
         else:
             Exception("Invalid optimizer provided")
         
